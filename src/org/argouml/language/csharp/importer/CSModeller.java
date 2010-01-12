917bb873405cd3ff0d537580795ecd127f28a8ec
Linus Tolke <linus@tigris.org>
2010-01-12 18:11:03 +0000

    Added EPL License header.
    git-svn-id: http://argouml-csharp.tigris.org/svn/argouml-csharp/trunk@282 79c8a056-b31d-0410-8776-8fca8d707a63

--- src/org/argouml/language/csharp/importer/CSModeller.java
+++ src/org/argouml/language/csharp/importer/CSModeller.java
@@ -1,4 +1,18 @@
-// TODO: What copyright header and license belongs here?
+/* $Id$
+ *****************************************************************************
+ * Copyright (c) 2009 Contributors - see below
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    tfmorris
+ *****************************************************************************
+ *
+ * Some portions of this file was previously release using the BSD License:
+ */
+
 package org.argouml.language.csharp.importer;
 
 import java.util.Collection;
